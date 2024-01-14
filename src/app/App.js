52ca5aa7448ff1df2fcf039f9c5b0e43395ce958
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-01-14 21:37:25 +0600

    init project and added deps

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material";
+
+const HeaderContainer = styled("div")(() => ({
+  borderRadius: "17px",
+  backgroundColor: "121825",
+  padding: "16px 24px"
+}));
+function App() {
+  return <HeaderContainer>
+    hello
+  </HeaderContainer>;
+}
+
+export default App;
